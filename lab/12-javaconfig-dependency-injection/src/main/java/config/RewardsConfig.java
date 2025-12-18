c4ba300605b1475d0c855eba6da0301a901191fc
Kaelyn S. <kaelyn.strezze@proton.me>
2025-12-18 16:34:03 -0500

    Finished Module 2 of Spring Framework Essentials course

--- lab/12-javaconfig-dependency-injection/src/main/java/config/RewardsConfig.java
+++ lab/12-javaconfig-dependency-injection/src/main/java/config/RewardsConfig.java
@@ -1,5 +1,18 @@
 package config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import rewards.RewardNetwork;
+import rewards.internal.RewardNetworkImpl;
+import rewards.internal.account.AccountRepository;
+import rewards.internal.account.JdbcAccountRepository;
+import rewards.internal.restaurant.JdbcRestaurantRepository;
+import rewards.internal.restaurant.RestaurantRepository;
+import rewards.internal.reward.JdbcRewardRepository;
+import rewards.internal.reward.RewardRepository;
+
 import javax.sql.DataSource;
 
 /**
@@ -42,9 +55,44 @@ import javax.sql.DataSource;
  *   not an implementation.
  */
 
+@Configuration
 public class RewardsConfig {
 
 	// Set this by adding a constructor.
 	private DataSource dataSource;
 
+	@Autowired
+	public RewardsConfig(DataSource ds) {
+		this.dataSource = ds;
+	}
+
+	@Bean
+	public AccountRepository accountRepository() {
+		JdbcAccountRepository repo = new JdbcAccountRepository();
+		repo.setDataSource(this.dataSource);
+		return repo;
+	}
+
+	@Bean
+	public RestaurantRepository restaurantRepository() {
+		JdbcRestaurantRepository repo = new JdbcRestaurantRepository();
+		repo.setDataSource(this.dataSource);
+		return repo;
+	}
+
+	@Bean
+	public RewardRepository rewardRepository() {
+		JdbcRewardRepository repo = new JdbcRewardRepository();
+		repo.setDataSource(this.dataSource);
+		return repo;
+	}
+
+	@Bean
+	public RewardNetwork rewardNetwork() {
+		return new RewardNetworkImpl(
+				this.accountRepository(),
+				this.restaurantRepository(),
+				this.rewardRepository()
+		);
+	}
 }
